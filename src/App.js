21ca0e5eac3f0b37417efea23a36b6999736a821
pharaohtechsolutions <fardinhojat8@gmail.com>
2023-08-07 21:25:23 -0400

    still setting up events

--- src/App.js
+++ src/App.js
@@ -3,41 +3,43 @@ import {useState} from "react";
 import ListView from './components/ListView.js';
 import {DataBaseService} from './services/DataBaseService.js'
 
-
-const onSearchByCardName = () => {
-  DataBaseService.searchForCardByName(document.getElementById("card-name").value);
-}
-
-const onSearchBySetName = () => {
-  DataBaseService.searchForSetByName(document.getElementById("set-name").value);
-}
-
-const onSearchClick = (event) => 
-{
-  switch (event.target["id"])
-  {
-    case "set-name-button":
-      onSearchBySetName();
-      break;
-    case "card-name-button":
-      onSearchByCardName();
-      break;
-    default:
-      console.log("Bad input")
-      break;
-  }
-}
-
 const App = () => 
 {
-  const [cardNames, onSearchByCardName] = useState([]);
-  const [setNames, onSearchBySetName] = useState([]);
+  const [cardNames, setCardNames] = useState([]);
+  const [setNames, setCardSetName] = useState([]);
+  const [searchResults, setSearchResults] = useState(["sahku","mah","bowss"]);
+
+  const onSearchByCardName = () => {
+    DataBaseService.searchForCardByName(document.getElementById("card-name").value);
+    // setCardNames()
+    setSearchResults()
+  }
+  
+  const onSearchBySetName = () => {
+    DataBaseService.searchForSetByName(document.getElementById("set-name").value);
+    // setCardSetName()
+    setSearchResults()
+  }
+
+  const onSearchClick = (event) => 
+  {
+    switch (event.target["id"])
+    {
+      case "set-name-button":
+        onSearchBySetName();
+        break;
+      case "card-name-button":
+        onSearchByCardName();
+        break;
+      default:
+        console.log("Bad input")
+        break;
+    }
+  }
 
   return (
     <div className="App">
-      <h2>
-        MTG Deck Builder
-      </h2>
+      <h2>MTG Deck Builder</h2>
       <label htmlFor="set-name">Name of MTG Deck</label>
       <input type="text" id="set-name" name="set-name" minLength="25" maxLength="50"/>
       <button id="set-name-button" onClick={onSearchClick}>Search Set Name</button>
@@ -45,8 +47,8 @@ const App = () =>
       <input type="text" id="card-name" name="card-name" minLength="15" maxLength="25"/>
       <button id="card-name-button" onClick={onSearchClick}>Search Card Name</button>
 
-    <ListView > 
-    </ListView>
+    <ListView searchResults={searchResults}/> 
+    
     </div>
   );
 }
--- src/components/ListView.js
+++ src/components/ListView.js
@@ -3,7 +3,10 @@ import './ListView.css';
 const ListView = (props) => {
     return (
         <div className="ListView"> 
-            {}
+            <ul>
+                {props.searchResults.length != 0 ? <p>No results</p> : 
+                props.searchResults.map(e => <li>{e}</li>)}
+            </ul>
         </div>
     );
 }
