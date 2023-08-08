9203c6cb8e7c04ffc41f4d683d5949a1690e91fc
pharaohtechsolutions <fardinhojat8@gmail.com>
2023-08-08 13:41:37 -0400

    DataBaseService.js filling out fetch() calls

--- src/services/DataBaseService.js
+++ src/services/DataBaseService.js
@@ -1,6 +1,6 @@
 const searchForCardByName = (cardName, callbackFunction) =>
 {
-    let requestObject = {method: "POST", headers: {"Content-Type":"application/json"}, body: JSON.stringify({data:"helloworld"})}
+    let requestObject = {method: "POST", headers: {"Content-Type":"application/json"}, body: JSON.stringify({cardName: cardName})}
     fetch("http://localhost:8080/search-card-by-name", requestObject)
     .then(response => {
         response.json()
@@ -12,7 +12,7 @@ const searchForCardByName = (cardName, callbackFunction) =>
 
 const searchForSetByName = (setName, callbackFunction) =>
 {
-    let requestObject = {method: "POST", headers: {"Content-Type":"application/json"}, body: JSON.stringify({data:"helloworld"})}
+    let requestObject = {method: "POST", headers: {"Content-Type":"application/json"}, body: JSON.stringify({setName: setName})}
     fetch("http://localhost:8080/search-set-by-name", requestObject)
     .then(response => {
         response.json()
