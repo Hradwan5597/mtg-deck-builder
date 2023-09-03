8c5af978d042491c94c62179b38d39519206b246
pharaohtechsolutions <fardinhojat8@gmail.com>
2023-09-03 02:37:21 -0400

    pagination wip

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -7,6 +7,9 @@ const ListView = (props) => {
                 {props.searchResults.length == 0 ? <p>No results</p> : 
                 props.searchResults.map(e => <li>{e}</li>)}
             </ul>
+            <div className="pagination">
+                1 2 3 4 5
+            </div>
         </div>
     );
 }
