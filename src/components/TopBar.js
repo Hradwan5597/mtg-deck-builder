ccf08b449fd4d5fdbadd5b3b4ab439c964744739
pharaohtechsolutions <fardinhojat8@gmail.com>
2023-11-07 11:42:31 -0500

    installed fontawesome, cartholdsitems indicator added to shopping cart button

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,17 +1,22 @@
 import './TopBar.css';
 import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart, faCircle } from '@fortawesome/free-solid-svg-icons';
 
 const TopBar = (props) =>
-{
-
-    const [shoppingCart, setShoppingCartItem] = useState([])
+{   
+    const [cartHoldsItems, setCartHoldsItems] = useState(false);
 
     return(
         <div className="TopBar grid-container">
             <h2>MTG Deck Builder</h2>
             <div className="user-controls">
                 <button id="cart" onClick={props.onShoppingCartClick}>
-                    Cart
+                    <span className='fa-layers fa-fw'>
+                        <FontAwesomeIcon icon={faShoppingCart} />
+                        {cartHoldsItems && <FontAwesomeIcon icon={faCircle} color="orange" transform="right-20 down-20"/>}
+                        
+                    </span>
                 </button>
             </div>
             
